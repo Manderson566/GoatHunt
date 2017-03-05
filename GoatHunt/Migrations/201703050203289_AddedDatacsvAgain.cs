fd583a11585fcbd4a723c72f6ee98a644c4a112b
Michael Anderson <Micmail566@gmail.com>
2017-03-04 21:04:15 -0500

    Added another Migration

---
+++ GoatHunt/Migrations/201703050203289_AddedDatacsvAgain.cs
@@ -0,0 +1,16 @@
+namespace GoatHunt.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedDatacsvAgain : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
